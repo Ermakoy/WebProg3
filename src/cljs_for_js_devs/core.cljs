51a900c71790f39fb1b35854e82ed223315a2404
ermakoy <ermak0y@yandex-team.ru>
2019-03-31 21:43:07 +0300

    first clojurescript example

---
+++ src/cljs_for_js_devs/core.cljs
@@ -0,0 +1,9 @@
+(ns cljs-for-js-devs.core)
+
+(def canvas (.getElementById js/document "drawing1"))
+(def ctx (.getContext canvas "2d"))
+
+(set! (.-fillStyle ctx) "rgb(255,0,0)")
+(.beginPath ctx)
+(.rect ctx 100 100 50 30)
+(.fill ctx)
